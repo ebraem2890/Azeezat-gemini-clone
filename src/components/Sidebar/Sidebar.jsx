3a115c95ab074f05abfbf229276e333818821fbe
Azeezat <ebraem2890@gmail.com>
2025-09-18 12:24:32 +0100

    Added more logic

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 const Sidebar = () => {
 
     const [extended, setExtended] = useState(false)
+    const { onSent, prevPrompts, setRecentPrompt } = useContext(Context)
 
     return (
         <div className='sidebar'>
             <div className="top">
-                <img onClick={() => setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt='' />
+                <img onClick={() => setExtended(prev => !prev)} className='menu' src={assets.menu_icon} alt='' />
                 <div className="new-chat">
                     <img src={assets.plus_icon} alt='' />
                     {extended ? <p>New Chat</p> : null}
@@ -18,10 +20,14 @@ const Sidebar = () => {
                     ?
                     <div className="recent">
                         <p className="recent-title">Recent</p>
-                        <div className="recent-entry">
-                            <img src={assets.message_icon} alt='' />
-                            <p>What is react ...</p>
-                        </div>
+                        {prevPrompts.map((item, index) => {
+                            return (
+                                <div className="recent-entry">
+                                    <img src={assets.message_icon} alt='' />
+                                    <p>{item.slice(0,18)} ...</p>
+                                </div>
+                            )
+                        })}
                     </div>
                     : null
                 }
@@ -29,15 +35,15 @@ const Sidebar = () => {
             <div className="bottom">
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} alt='' />
-                   {extended?<p>Help</p> : null} 
+                    {extended ? <p>Help</p> : null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} alt='' />
-                    {extended? <p>Activity</p> : null}
+                    {extended ? <p>Activity</p> : null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.setting_icon} alt='' />
-                    {extended? <p>Setting</p> : null}
+                    {extended ? <p>Setting</p> : null}
                 </div>
             </div>
         </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -14,7 +14,9 @@ const ContextProvider = (props) => {
     const [resultData, setResultData] = useState("");
 
     const delayPara = (index, nextWord) => {
-
+       setTimeout(function () {
+          setResultData(prev => prev + nextWord);
+       }, 75*index)
     }
 
     const onSent = async (prompt) => {
@@ -23,8 +25,26 @@ const ContextProvider = (props) => {
         setLoading(true)
         setShowResult(true)
         setRecentPrompt(input)
+        setPrevPrompts(prev => [...prev,input])
        const response = await runChat(input)
-       setResultData(response)
+       let responseArray = response.split("**");
+       let newResponse = "";
+       for(let i=0; i< responseArray.length; i++)
+       {
+           if (i ===0 || i%2 !== 1) {
+              newResponse += responseArray[i];
+           }
+           else{
+              newResponse += "<b>" + responseArray[i] + "</b>";
+           }
+       }
+       let newResponse2 = newResponse.split("*").join("</br")
+       let newResponseArray = newResponse2.split(" ");
+       for(let i = 0;i < newResponseArray.length; i++)
+       {
+          const nextWord = newResponseArray[i];
+          delayPara(i, nextWord + " ")
+       }
        setLoading(false)
        setInput("")
     }
